dd4075354f8262f981d180378898c0cab22e01b8
monty-jose <monti.jose04@gmail.com>
2020-02-17 14:09:17 -0300

    archivo de configuracion para que inicie el sistema

--- application/controllers/usuario.php
+++ application/controllers/usuario.php
@@ -11,6 +11,7 @@ class Usuario extends MY_Controller {
    */
   public function __construct() {
     parent::__construct();
+   //$this->session->sess_destroy();
   }
 
   // --------------------------------------------------------------------
---
+++ application/logs/log-2020-02-17.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-02-17 13:42:17 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:43:46 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:43:53 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:44:22 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:45:49 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:46:20 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:53:27 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:53:30 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:53:31 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:54:52 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:54:57 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:54:59 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:54:59 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:54:59 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:54:59 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:46 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:50 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:55:59 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:56:00 --> Severity: Warning  --> Missing argument 1 for Sindicatos_model::obtenerEmpresa(), called in D:\SistemaIntegral\htdocs\grupo-provision\application\controllers\archivo.php on line 310 and defined D:\SistemaIntegral\htdocs\grupo-provision\application\models\sindicatos_model.php 60
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: archivos_validos D:\SistemaIntegral\htdocs\grupo-provision\application\controllers\archivo.php 348
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: archivos D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 9
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_archivo D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 33
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_empresa D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 66
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_empresa D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 66
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_empresa D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 66
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_departamento D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 82
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: departamentos D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 86
+ERROR - 2020-02-17 13:56:00 --> Severity: Warning  --> Invalid argument supplied for foreach() D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 86
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_departamento D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 95
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: departamentos D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 107
+ERROR - 2020-02-17 13:56:00 --> Severity: Warning  --> Invalid argument supplied for foreach() D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 107
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: id_departamento D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 116
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: fecha_vigencia D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 140
+ERROR - 2020-02-17 13:56:00 --> Severity: Notice  --> Undefined variable: observacion D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 159
+ERROR - 2020-02-17 13:56:00 --> Severity: Warning  --> Invalid argument supplied for foreach() D:\SistemaIntegral\htdocs\grupo-provision\application\views\archivo\archivo_view.php 195
+ERROR - 2020-02-17 13:56:12 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:56:13 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
+ERROR - 2020-02-17 13:56:16 --> Severity: Notice  --> Only variable references should be returned by reference D:\SistemaIntegral\htdocs\grupo-provision\system\core\Common.php 257
